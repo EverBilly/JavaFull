3b8af8f9759bb0022be00ac6e2294e1f670a855e
Ever Cifuentes <everbilly35@gmail.com>
2020-10-17 15:27:22 -0600

    Video 40 Herencia 1, ejemplo de Furgoneta y el uso de la palabra reservada super()

--- pildoras/poo/Furgoneta.java
+++ pildoras/poo/Furgoneta.java
@@ -1,11 +1,12 @@
 public class Furgoneta extends Coche {
 
-    private int capacidad_carga, plazas_extra;
+    private int capacidad_carga;
+    private int plazas_extra;
 
     public Furgoneta(int plazas_extra, int capacidad_carga) {
         //Super llama al constructor de la clase padre
         //EJ: public Coche() { mas lo que tiene el constructor }
-        super();
+        super();//Da un estado inicial a la clase Coche()
         this.capacidad_carga = capacidad_carga;
         this.plazas_extra = plazas_extra;
     }
